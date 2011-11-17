0f364c96202d26af394bb54cbdd7ad9186a3e2be
Damian Gryski <damian@gryski.com>
2011-11-17 10:11:25 +0100

    Handle stdin for lua version too.

--- spark.lua
+++ spark.lua
@@ -1,12 +1,24 @@
+
 bars = {[0] = "__", "▁ " , "▂ " , "▃ " , "▄ " , "▅ ", "▆ ", "▇ ", "█ "}
+vals = {}
+
+if #arg == 0 then
+    i = 1
+    for line in io.lines() do
+        for n in string.gmatch(line, "[%d.]+") do
+            vals[i] = n 
+            i = i + 1
+        end
+    end
+else
+    vals = arg
+end
 
 max = 0
 min = 3e38 -- max of a float32
 
-vals = {}
-
-for i=1,#arg do
-    vals[i]=arg[i] + 0 -- coerce to number
+for i=1,#vals do
+    vals[i] = tonumber(vals[i])
     if max < vals[i] then max = vals[i] end
     if min > vals[i] then min = vals[i] end
 end
