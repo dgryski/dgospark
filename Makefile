18147d2cd4401a865de3896f4ae0d93e5e41cf30
Damian Gryski <damian@gryski.com>
2011-11-17 01:10:12 +0100

    Tiny utility to draw sparklines your command line.

---
+++ Makefile
@@ -0,0 +1,6 @@
+include $(GOROOT)/src/Make.inc
+
+TARG=spark
+GOFILES=spark.go
+
+include $(GOROOT)/src/Make.cmd
---
+++ spark.go
@@ -0,0 +1,74 @@
+// Sparklines for your command line
+// Inspired by https://github.com/holman/spark
+// Copyright (c) 2011 Damian Gryski <damian@gryski.com>
+// Licensed under the GPLv3 or, at your option, any later version
+
+package main
+
+import (
+	"bufio"
+	"flag"
+        "math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func graph(vals []float32) string {
+
+	max := float32(0.0)
+	min := float32(math.MaxFloat32)
+
+	for _, v := range vals {
+		if max < v {
+			max = v
+		}
+		if min > v {
+			min = v
+		}
+	}
+
+
+	graph := ""
+	// the last box doesn't look great on my mac :(
+	bars := []string{"__", "▁ " , "▂ " , "▃ " , "▄ " , "▅ ", "▆ ", "▇" , "█ "}
+	scale := float32(len(bars)-1) / (max - min)
+	for _, v := range vals {
+		h := int(scale * float32(v-min))
+		graph = graph + bars[h];
+	}
+
+	return graph
+}
+
+func main() {
+
+	flag.Parse()
+
+	var vals []float32
+
+	if flag.NArg() != 0 {
+		// scan from parameters
+		for _, s := range flag.Args() {
+			fl, _ := strconv.Atof32(s)
+			vals = append(vals, fl)
+		}
+	} else {
+		// scan from stdin
+		br := bufio.NewReader(os.Stdin)
+		for {
+			s, err := br.ReadString('\n')
+			if s == "" && err != nil {
+				break
+			}
+			fields := strings.Fields(s)
+
+			for _, f := range fields {
+				fl, _ := strconv.Atof32(f)
+				vals = append(vals, fl)
+			}
+		}
+	}
+
+	os.Stdout.Write([]byte("|" +  graph(vals) + "|\n"))
+}
